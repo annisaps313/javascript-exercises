78b07a7643a886ed93349465acf72923db3875fa
annisaps313 <annisaps313@gmail.com>
2026-04-05 16:06:18 +0700

    Add palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+	const cleanedStr = str
+		.toLowerCase()
+		.split('')
+		.filter(item => alphabet.includes(item))
+		.join('')
 
+	const reversedStr = cleanedStr.split('').reverse().join('');
+	return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
