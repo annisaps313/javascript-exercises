6debae9b4e7e0e015ec8f441bd0d5b24482c1a3c
annisaps313 <annisaps313@gmail.com>
2026-04-05 18:31:56 +0700

    Add fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let arr = [0, 1];
 
+	if (num < 0) {
+		return "OOPS";
+	}
+
+	for (let i = 2; i <= num; i++){
+		arr.push(arr[i - 1] + arr[i - 2]);
+	}
+
+	return arr[num];
 };
 
 // Do not edit below this line
